802e72df198eb4fa09de813c62d2e68de9f60fb9
Technik <aniket.97.mane@gmail.com>
2024-10-04 00:51:32 +0530

    Managing neovim config using default.nix

---
+++ functions/fromGitHub.nix
@@ -0,0 +1,12 @@
+{pkgs, user, repo, ref ? "HEAD", rev, buildScript ? ":"}:
+
+pkgs.vimUtils.buildVimPlugin {
+  pname = "${pkgs.lib.strings.sanitizeDerivationName repo}";
+  version = ref;
+  src = builtins.fetchGit {
+    url = "https://github.com/${user}/${repo}.git";
+    rev = rev;
+    inherit ref;
+  };
+  inherit buildScript;
+}
