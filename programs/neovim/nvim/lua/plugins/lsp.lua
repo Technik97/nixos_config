98a38b5f7dc64b307315c6365dec1108dd9d93ce
Technik <aniket.97.mane@gmail.com>
2024-10-05 13:50:25 +0530

    File cleanup

--- programs/neovim/nvim/lua/plugins/lsp.lua
+++ programs/neovim/nvim/lua/plugins/lsp.lua
@@ -117,14 +117,10 @@ return { -- LSP Configuration & Plugins
             runtime = { version = 'LuaJIT' },
             workspace = {
               checkThirdParty = false,
-              -- Tells lua_ls where to find all the Lua files that you have loaded
-              -- for your neovim configuration.
               library = {
                 '${3rd}/luv/library',
                 unpack(vim.api.nvim_get_runtime_file('', true)),
               },
-              -- If lua_ls is really slow on your computer, you can try this instead:
-              -- library = { vim.env.VIMRUNTIME },
             },
             completion = {
               callSnippet = 'Replace',
@@ -150,8 +146,6 @@ return { -- LSP Configuration & Plugins
     -- Ensure the servers and tools above are installed
     require('mason').setup()
 
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -159,9 +153,6 @@ return { -- LSP Configuration & Plugins
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
