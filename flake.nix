cd3d416d1cb0c4e92dbdf78ee725bd8e0ff4c5e5
Technik <aniket.97.mane@gmail.com>
2024-10-06 01:33:04 +0530

    Fixed error where firefox addons were not available

--- flake.nix
+++ flake.nix
@@ -3,15 +3,19 @@
 
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-24.05";
-	home-manager.url = "github:nix-community/home-manager/release-24.05";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    	home-manager.url = "github:nix-community/home-manager/release-24.05";
+	    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	firefox-addons = {
+      	    url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+            inputs.nixpkgs.follows = "nixpkgs";
+   	 };
     };
 
-    outputs = { self, nixpkgs, home-manager,  ... }: 
+    outputs = { self, nixpkgs, home-manager,  ... }@inputs: 
         let 
           lib = nixpkgs.lib; 
- 	  system = "x86_64-linux";
-	  pkgs = nixpkgs.legacyPackages.${system};
+     	  system = "x86_64-linux";
+	      pkgs = nixpkgs.legacyPackages.${system};
         in {
            nixosConfigurations = {
               nixos = lib.nixosSystem {
@@ -20,10 +24,11 @@
               };
            };
 	   homeConfigurations = {
-               technik = home-manager.lib.homeManagerConfiguration {
-	          inherit pkgs;
-		  modules = [ ./home.nix ];
-	       };
-	   };
-        };
+                technik = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                extraSpecialArgs = { inherit inputs; };
+		        modules = [ ./home.nix ];
+            };
+	    };
+    };
 }
