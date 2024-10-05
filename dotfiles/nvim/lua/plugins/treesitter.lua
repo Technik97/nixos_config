b08e28d34d13aaf531b5acd37d1e46b747168cbe
Technik <aniket.97.mane@gmail.com>
2024-10-05 16:52:11 +0530

    Created dotfiles folder

---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 
+          'bash',
+          'lua', 
+          'luadoc',  
+          'vim', 
+          'vimdoc',
+          'rust',
+      },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  }
