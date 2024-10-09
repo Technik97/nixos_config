7919bb215a9d12be6d246b1fb5581eab212e2d8c
Technik <aniket.97.mane@gmail.com>
2024-10-10 02:16:50 +0530

    Added kitty terminal

--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@
     ./applications/tui/shell 
     ./applications/tui/neovim
     ./applications/gui/browsers/firefox
+    ./applications/tui/kitty
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
